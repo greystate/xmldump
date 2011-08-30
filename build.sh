72ad78d13af80786cff7c771cdf08403d82c4756
Chriztian Steinmeier <chriztian@steinmeier.dk>
2011-08-30 21:35:00 +0200

    Excluding .DS_Store files from ZIP

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ fi
 xsltproc --novalid --output package/XMLDump.xslt lib/freezeEntities.xslt src/XMLDump.xslt 
 xsltproc --novalid --xinclude --output package/package.xml lib/freezeEntities.xslt src/package.xml 
 
-zip -j dist/XMLDump package/*
+zip -j dist/XMLDump package/* -x \*.DS_Store
