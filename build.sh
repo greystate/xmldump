b8cffc6a5f01920ea3b97e74da8821c81104b785
Chriztian Steinmeier <chriztian@steinmeier.dk>
2011-09-15 19:03:12 +0300

    Adding comments to the build script; adding step to copy release XSLT to dist directory for upgrade purposes

--- build.sh
+++ build.sh
@@ -1,13 +1,22 @@
+# Create the dist directory if needed
 if [[ ! -d dist ]]
 	then mkdir dist
 fi
+# Likewise, create the package dir
 if [[ ! -d package ]]
 	then mkdir package
 fi
 
-xsltproc --novalid --output package/XMLDump.xslt lib/freezeEntities.xslt src/XMLDump.xslt 
+# Transform the development XSLT into the release file
+xsltproc --novalid --output package/XMLDump.xslt lib/freezeEntities.xslt src/XMLDump.xslt
+
+# Transform the package.xml file, pulling in the README
 xsltproc --novalid --xinclude --output package/package.xml lib/freezeEntities.xslt src/package.xml 
 
+# Build the ZIP file 
 zip -j dist/XMLDump package/* -x \*.DS_Store
+
+# Copy the release XSLT into the dist dir for upgraders
+cp package/XMLDump.xslt dist/XMLDump.xslt
 
 # coffee  -o package/ -c src/xmldump.coffee
