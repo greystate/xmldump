b8617e0e2d525bc4ddf496766e8bb5947652ad5d
Chriztian Steinmeier <chriztian@steinmeier.dk>
2013-04-11 00:56:44 +0200

    Update build.sh to use version in zipfile-name and switch entities before/after running

--- build.sh
+++ build.sh
@@ -7,6 +7,20 @@ if [[ ! -d package ]]
 	then mkdir package
 fi
 
+# Get the current version
+VERSION=`grep -o ' packageVersion \"\(.*\)\"' src/version.ent | awk '{print $2}' | sed 's/"//g'`
+
+# Make sure to use the PRODUCTION entities
+UMBOFF="RELEASE \"IGNORE\""
+UMBON="RELEASE \"INCLUDE\""
+
+TMOFF="TESTING \"IGNORE\""
+TMON="TESTING \"INCLUDE\""
+
+sed -i "" "s/$UMBOFF/$UMBON/" src/compatibility.ent
+sed -i "" "s/$TMON/$TMOFF/" src/compatibility.ent
+
+
 # Transform the development XSLT into the release file
 xsltproc --novalid --output package/XMLDump.xslt lib/freezeEntities.xslt src/XMLDump.xslt
 
@@ -14,10 +28,14 @@ xsltproc --novalid --output package/XMLDump.xslt lib/freezeEntities.xslt src/XML
 xsltproc --novalid --xinclude --output package/package.xml lib/freezeEntities.xslt src/package.xml 
 
 # Build the ZIP file 
-zip -j dist/XMLDump package/* -x \*.DS_Store
+zip -j "dist/XMLDump-$VERSION.zip" package/* -x \*.DS_Store
 
 # Copy the release XSLT into the dist dir for upgraders
 cp package/XMLDump.xslt dist/XMLDump.xslt
+
+# Go back to DEVELOPMENT entities again
+sed -i "" "s/$UMBON/$UMBOFF/" src/compatibility.ent
+sed -i "" "s/$TMOFF/$TMON/" src/compatibility.ent
 
 # This is a future enhancement in the works ...
 # coffee  -o package/ -c src/xmldump.coffee
